979c1f3009c4d6143e4066d9d707181659dcbb98
SPBNAVIS\o.kabirova <lianirvi@gmail.com>
2017-06-01 17:06:08 +0300

    design fix3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,18 @@ import { MdDialogRef } from '@angular/material';
 })
 export class AppComponent {
   isDarkTheme = false;
+  name = 'Ганнибал Лектор Кулинарович';
   nodes = [
     {
       id: 1,
       name: 'root1',
       children: [
         { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
+        { id: 3, name: 'child2' },
+        { id: 8, name: 'child2' },
+        { id: 9, name: 'child2' },
+        { id: 10, name: 'child2' },
+        { id: 11, name: 'child2' }
       ]
     },
     {
@@ -26,7 +31,62 @@ export class AppComponent {
           id: 6,
           name: 'child2.2',
           children: [
-            { id: 7, name: 'subsub' }
+            { id: 7, name: 'subsub' },
+            { id: 12, name: 'subsub' },
+            { id: 13, name: 'subsub' },
+            { id: 14, name: 'subsub' },
+            { id: 15, name: 'subsub' },
+            { id: 16, name: 'subsub' }
+          ]
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' },
+        { id: 8, name: 'child2' },
+        { id: 9, name: 'child2' },
+        { id: 10, name: 'child2' },
+        { id: 11, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' },
+            { id: 12, name: 'subsub' },
+            { id: 13, name: 'subsub' },
+            { id: 14, name: 'subsub' },
+            { id: 15, name: 'subsub' },
+            { id: 16, name: 'subsub' }
+          ]
+        }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' },
+            { id: 12, name: 'subsub' },
+            { id: 13, name: 'subsub' },
+            { id: 14, name: 'subsub' },
+            { id: 15, name: 'subsub' },
+            { id: 16, name: 'subsub' }
           ]
         }
       ]
